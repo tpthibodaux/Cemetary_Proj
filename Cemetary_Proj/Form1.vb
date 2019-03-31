119c099907707d1d5e65cfd1ab3a0db767882bb3
tpthibodaux <tpthibodaux@gmail.com>
2019-03-31 08:35:21 -0500

    Scroll bar and new buttons

--- Cemetary_Proj/Form1.vb
+++ Cemetary_Proj/Form1.vb
@@ -7,4 +7,16 @@
         Dim CemeteryColumn = SearchDGV.Columns("Cemetery")
         CemeteryColumn.MinimumWidth = (SearchDGV.Width * 0.4)
     End Sub
+
+    Private Sub Button_Delete_Click(sender As Object, e As EventArgs) Handles Button_Delete.Click
+
+    End Sub
+
+    Private Sub btnNewCard_Click(sender As Object, e As EventArgs) Handles btnNewCard.Click
+
+    End Sub
+
+    Private Sub Button_Search_Click(sender As Object, e As EventArgs) Handles Button_Search.Click
+
+    End Sub
 End Class
