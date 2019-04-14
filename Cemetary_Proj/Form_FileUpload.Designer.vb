42b0e352fd9bc105cfe7cbb1c28e5550f77cfed1
Ty13R <tpthibodaux@gmail.com>
2019-04-14 18:30:09 -0500

    File upload and viewing capabilities

---
+++ Cemetary_Proj/Form_FileUpload.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_FileUpload
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.txtFileName = New System.Windows.Forms.TextBox()
+        Me.txtDescr = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(100, 655)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(132, 23)
+        Me.btnSearch.TabIndex = 0
+        Me.btnSearch.Text = "Search for Image"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(250, 655)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(132, 23)
+        Me.btnUpload.TabIndex = 1
+        Me.btnUpload.Text = "Upload Image"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'txtFileName
+        '
+        Me.txtFileName.Location = New System.Drawing.Point(100, 603)
+        Me.txtFileName.Name = "txtFileName"
+        Me.txtFileName.Size = New System.Drawing.Size(506, 20)
+        Me.txtFileName.TabIndex = 3
+        '
+        'txtDescr
+        '
+        Me.txtDescr.Location = New System.Drawing.Point(100, 629)
+        Me.txtDescr.Name = "txtDescr"
+        Me.txtDescr.Size = New System.Drawing.Size(153, 20)
+        Me.txtDescr.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(18, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(588, 570)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 606)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "File Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 632)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Description:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(405, 655)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(132, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form_FileUpload
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 690)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDescr)
+        Me.Controls.Add(Me.txtFileName)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Name = "Form_FileUpload"
+        Me.Text = "Form_FileUpload"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents txtDescr As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Cemetary_Proj/Form_ViewFiles.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_ViewFiles
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.dgvFiles = New System.Windows.Forms.DataGridView()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DescrDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ImgDataGridViewImageColumn = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.GrabImagesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.GraveSampleDataSet3 = New Cemetary_Proj.GraveSampleDataSet3()
+        Me.GrabImagesTableAdapter = New Cemetary_Proj.GraveSampleDataSet3TableAdapters.GrabImagesTableAdapter()
+        Me.btnTxt = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.dgvFiles, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GrabImagesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GraveSampleDataSet3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvFiles
+        '
+        Me.dgvFiles.AutoGenerateColumns = False
+        Me.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvFiles.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NameDataGridViewTextBoxColumn, Me.DescrDataGridViewTextBoxColumn, Me.ImgDataGridViewImageColumn})
+        Me.dgvFiles.DataSource = Me.GrabImagesBindingSource
+        Me.dgvFiles.Location = New System.Drawing.Point(2, 3)
+        Me.dgvFiles.Name = "dgvFiles"
+        Me.dgvFiles.Size = New System.Drawing.Size(1045, 315)
+        Me.dgvFiles.TabIndex = 0
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "File Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        Me.NameDataGridViewTextBoxColumn.Width = 200
+        '
+        'DescrDataGridViewTextBoxColumn
+        '
+        Me.DescrDataGridViewTextBoxColumn.DataPropertyName = "descr"
+        Me.DescrDataGridViewTextBoxColumn.HeaderText = "File Description"
+        Me.DescrDataGridViewTextBoxColumn.Name = "DescrDataGridViewTextBoxColumn"
+        Me.DescrDataGridViewTextBoxColumn.Width = 200
+        '
+        'ImgDataGridViewImageColumn
+        '
+        Me.ImgDataGridViewImageColumn.DataPropertyName = "img"
+        Me.ImgDataGridViewImageColumn.HeaderText = "Image"
+        Me.ImgDataGridViewImageColumn.Name = "ImgDataGridViewImageColumn"
+        Me.ImgDataGridViewImageColumn.Width = 600
+        '
+        'GrabImagesBindingSource
+        '
+        Me.GrabImagesBindingSource.DataMember = "GrabImages"
+        Me.GrabImagesBindingSource.DataSource = Me.GraveSampleDataSet3
+        '
+        'GraveSampleDataSet3
+        '
+        Me.GraveSampleDataSet3.DataSetName = "GraveSampleDataSet3"
+        Me.GraveSampleDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'GrabImagesTableAdapter
+        '
+        Me.GrabImagesTableAdapter.ClearBeforeFill = True
+        '
+        'btnTxt
+        '
+        Me.btnTxt.Location = New System.Drawing.Point(150, 359)
+        Me.btnTxt.Name = "btnTxt"
+        Me.btnTxt.Size = New System.Drawing.Size(136, 26)
+        Me.btnTxt.TabIndex = 1
+        Me.btnTxt.Text = "Exit"
+        Me.btnTxt.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(691, 359)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(138, 26)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Delete File"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form_ViewFiles
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1050, 554)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnTxt)
+        Me.Controls.Add(Me.dgvFiles)
+        Me.Name = "Form_ViewFiles"
+        Me.Text = "Form_ViewFiles"
+        CType(Me.dgvFiles, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GrabImagesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GraveSampleDataSet3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgvFiles As DataGridView
+    Friend WithEvents GrabImagesBindingSource As BindingSource
+    Friend WithEvents GraveSampleDataSet3 As GraveSampleDataSet3
+    Friend WithEvents GrabImagesTableAdapter As GraveSampleDataSet3TableAdapters.GrabImagesTableAdapter
+    Friend WithEvents btnTxt As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DescrDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ImgDataGridViewImageColumn As DataGridViewImageColumn
+End Class
